bf86b96f19a2c8b85dac58da4647bbd6dd803592
Bulat95 <49592201+Bulat95@users.noreply.github.com>
2022-05-29 22:18:42 +0400

    first commit
    Решил 404 сменой версии tomcat, вывод страниц

---
+++ src/main/java/ru/batullin/bank/Cntr.java
@@ -0,0 +1,12 @@
+package ru.batullin.bank;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class Cntr {
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
